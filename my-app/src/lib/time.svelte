b6e58ac1621b58e7a348edff1898097ff91a7daa
Ivan <jicxer@gmail.com>
2023-12-10 20:01:31 -0500

    added a search bar

---
+++ my-app/src/lib/time.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { time, elapsed } from './time.js';
+
+	const formatter = new Intl.DateTimeFormat('en', {
+		hour12: true,
+		hour: 'numeric',
+		minute: '2-digit',
+		second: '2-digit'
+	});
+</script>
+
+
+<div class="time-widget">The time is {formatter.format($time)}</div>
+
+
+<style>
+    .time-widget {
+      font-size: 2rem;
+      color: #333;
+    }
+  </style>
+  
+
+
--- my-app/src/routes/dashboard/+page.svelte
+++ my-app/src/routes/dashboard/+page.svelte
@@ -1,17 +1,97 @@
-<script>
-    import Sidebar from '$lib/sidebar.svelte';
-    import Wip from '$lib/wip.svelte';
+<svelte:head>
+	<link rel="stylesheet" href="https://unpkg.com/mono-icons@1.0.5/iconfont/icons.css" >
+</svelte:head>
 
+<script>
+  import Sidebar from '$lib/sidebar.svelte';
+  import Wip from '$lib/wip.svelte';
+  import Time from '$lib/time.svelte';
+
+  let searchTerm = '';
+
+  function handleSearch() {
+  // Implement your search logic here
+    console.log('Searching for:', searchTerm);
+  }
   </script>
   
+
+  
+  <div class="container">
+    <Sidebar />
+    <main class="content">
+      <div class = "title">
+        <h1>Account Dashboard</h1>
+        <Time/>
+
+      </div>
+      <div class="search-bar">
+        <input
+          type="text"
+          class="search-input"
+          placeholder="Search..."
+          bind:value={searchTerm}
+        />
+        <button on:click={handleSearch} class="search-button">
+          <i class="mi mi-search"><span class="u-sr-only"></span></i>
+        </button>
+      </div>
+      <div class="widgets-container">
+        <div class="flexbox"> <p class="flexbox-text">View Log Sources</p></div>
+        <div class="flexbox"><p class="flexbox-text">Temporary</p></div>
+        <div class="flexbox"><p class="flexbox-text">Placement</p></div>
+        <div class="flexbox"><p class="flexbox-text">Of</p></div>
+        <div class="flexbox flexbox-stretched"><p class="flexbox-text">Flexboxes</p></div>
+      </div>
+
+    </main>
+    <Wip/>
+  </div>
+
   <style>
 
-  /* @font-face {
-    font-family: 'Forum';
-    src: url('static/fonts/Forum-Regular.ttf') format('truetype');
-    font-weight: normal;
-    font-style: normal;
-    } */
+.search-bar {
+
+
+
+
+    margin-bottom: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    box-sizing: border-box;
+  }
+
+  .search-input {
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    width: 100%;
+    box-sizing: border-box;
+  }
+
+  .search-button {
+    padding: 10px;
+    border: none;
+    background-color: #4caf50;
+    color: white;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+  }
+
+  .search-button:hover {
+    background-color: black;
+  }
+
+    .title {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      margin-bottom: 10px;
+    }
 
     .container {
       display: flex;
@@ -59,29 +139,12 @@
     align-self: flex-start; /* Align this specific item to the top if other items are added */
   }
 
-  
+
   :global(body) {
     background-color: #d3d3d3; 
   }
 
 
   </style>
-  
-  <div class="container">
-    <Sidebar />
-    <main class="content">
-      <h1>Account Dashboard</h1>
-    
-      <div class="widgets-container">
-        <div class="flexbox"> <p class="flexbox-text">View Log Sources</p></div>
-        <div class="flexbox"><p class="flexbox-text">Temporary</p></div>
-        <div class="flexbox"><p class="flexbox-text">Placement</p></div>
-        <div class="flexbox"><p class="flexbox-text">Of</p></div>
-        <div class="flexbox flexbox-stretched"><p class="flexbox-text">Flexboxes</p></div>
-      </div>
-
-    </main>
-    <Wip/>
-  </div>
 
   
