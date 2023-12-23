d13f47d5c6845feac2b4b2550326e9d9c3646709
Jicxer <79224427+Jicxer@users.noreply.github.com>
2023-12-23 15:42:28 -0800

    Cert download route (#19) (#20)
    * Create CODEOWNER (#6)
    
    updated ruleset. removed 4 required approves
    
    * removed child directory,my-app
    
    * ssg choice
    
    * ssg choice
    
    * spa chosen
    
    * spa for login/auth
    
    * api path testing. priority is credential apipath
    
    * api path testing. priority is send bearer and make credential apipath
    
    * prepping for merge
    
    * added auth0 audience env var
    
    ---------
    
    Co-authored-by: Ende <30739491+endepointe@users.noreply.github.com>
    Co-authored-by: Kevin Huynh <kevinhuynh200289@gmail.com>
    Co-authored-by: Jicxer <kali@kali>

--- src/services/message.service.js
+++ src/services/message.service.js
@@ -64,3 +64,104 @@ export const getAdminResource = async () => {
     message.set(JSON.stringify(error, null, 2));
   }
 };
+import { writable } from "svelte/store";
+import { callExternalApi } from "./external-api.service";
+
+const apiServerUrl = import.meta.env.VITE_API_SERVER_URL;
+
+export const message = writable("");
+
+export const getPublicResource = async () => {
+  const config = {
+    url: `${apiServerUrl}/api/messages/public`,
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+    },
+  };
+
+  const { data, error } = await callExternalApi({ config });
+
+  if (data) {
+    message.set(JSON.stringify(data, null, 2));
+  }
+
+  if (error) {
+    message.set(JSON.stringify(error, null, 2));
+  }
+};
+
+export const getPrivateResource = async () => {
+  const config = {
+    url: `${apiServerUrl}/api/messages/private`,
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+    },
+  };
+  const { data, error } = await callExternalApi({ config });
+
+  if (data) {
+    message.set(JSON.stringify(data, null, 2));
+  }
+
+  if (error) {
+    message.set(JSON.stringify(error, null, 2));
+  }
+};
+
+export const getProtectedResource = async () => {
+  const config = {
+    url: `${apiServerUrl}/api/messages/protected`,
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+    },
+  };
+  const { data, error } = await callExternalApi({ config });
+
+  if (data) {
+    message.set(JSON.stringify(data, null, 2));
+  }
+
+  if (error) {
+    message.set(JSON.stringify(error, null, 2));
+  }
+};
+
+export const getPrivateScopedResource = async () => {
+    const config = {
+        url: `${apiServerUrl}/api/messages/private-scoped`,
+        method: "GET",
+        headers: {
+            "content-type": "application/json",
+        },
+    };
+    const {data, error} = await callExternalApi({config});
+    if (data) {
+        message.set(JSON.stringify(data, null, 2));
+    }
+    if (error) {
+        message.set(JSON.stringify(error, null, 2));
+    }
+};
+
+export const getAdminResource = async () => {
+  const config = {
+    url: `${apiServerUrl}/api/messages/admin`,
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+    },
+  };
+
+  const { data, error } = await callExternalApi({ config });
+
+  if (data) {
+    message.set(JSON.stringify(data, null, 2));
+  }
+
+  if (error) {
+    message.set(JSON.stringify(error, null, 2));
+  }
+};
